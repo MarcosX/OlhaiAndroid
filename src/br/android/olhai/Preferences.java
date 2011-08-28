bd1178e93724258ed34aeb7215adceafaaa254ef
Thiago Nascimento <thiagodnf@gmail.com>
2011-08-28 17:28:02 -0300

    Com esse commit, o usuário poderá saber qual universidade escolheu nas suas preferências

--- src/br/android/olhai/Preferences.java
+++ src/br/android/olhai/Preferences.java
@@ -2,12 +2,15 @@ package br.android.olhai;
 
 import br.android.olhai.R;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
  
-public class Preferences extends PreferenceActivity {
+public class Preferences extends PreferenceActivity implements OnSharedPreferenceChangeListener {
+
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +18,44 @@ public class Preferences extends PreferenceActivity {
 		super.onCreate(savedInstanceState);
 	    addPreferencesFromResource(R.xml.preferences);       
 	    
+	    for(int i=0;i<getPreferenceScreen().getPreferenceCount();i++){
+	    	initSummary(getPreferenceScreen().getPreference(i));
+	    }
+
     }
 	
-	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-	    Preference pref = findPreference(key);
+	 @Override 
+     protected void onResume(){
+         super.onResume();
+         // Set up a listener whenever a key changes             
+         getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+     }
 
-	    if (pref instanceof ListPreference) {
-	        //ListPreference listPref = (ListPreference) pref;
-	        //pref.setSummary(listPref.getEntry());
-	    }
-	}
+	
+	 public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) { 
+         updatePrefSummary(findPreference(key));
+     }
+
+     private void initSummary(Preference p){
+        if (p instanceof PreferenceCategory){
+             PreferenceCategory pCat = (PreferenceCategory)p;
+             for(int i=0;i<pCat.getPreferenceCount();i++){
+                 initSummary(pCat.getPreference(i));
+             }
+         }else{
+             updatePrefSummary(p);
+         }
+
+     }
+
+     private void updatePrefSummary(Preference p){
+         if (p instanceof ListPreference) {
+             ListPreference listPref = (ListPreference) p; 
+             p.setSummary(listPref.getEntry()); 
+         }        
+
+     }
+
 
 }
 
